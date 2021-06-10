b5878e6281411cc7db61ecfe811b9956edaa790e
irumvanselme <andesanselme@gmail.com>
2021-06-10 20:19:17 +0200

    Initialize auth methods

--- controllers/auth.controller.ts
+++ controllers/auth.controller.ts
@@ -8,15 +8,27 @@ import User from "../models/user.model"
 import { Controller } from "./index";
 
 class AuthController extends Controller {
+    login_validations = {
+        login: "required|string|min:3",
+        password: "required|string|min:3"
+    }
+
     async register (req: Request, res: Response) {
-        return res.send("Register here")
+        let { valid, errors } = super.validate(req.body, User.validations);
+        if (!valid) return res.status(400).send(errors)
+
+        let newTodo = await User.create(req.body)
     }
 
     async login (req: Request, res: Response) {
         try {
+            let { valid, errors } = super.validate(req.body, this.login_validations);
+            if (!valid) return res.status(400).send(errors)
+
             const user = await User.query.findOne().or([
-                { email: req.body.email },
-                { username: req.body.username },
+                { email: req.body.login },
+                { username: req.body.login },
+                { phone: req.body.login },
             ]);
 
             if (user && (await bcrypt.compareSync(req.body.password, user.password))) {
@@ -30,6 +42,18 @@ class AuthController extends Controller {
         } catch ( e ) {
             return res.status(500).send(e);
         }
+    }
+
+    async profile (req: Request, res: Response) {
+
+    }
+
+    async email_available (req: Request, res: Response) {
+
+    }
+
+    async username_available (req: Request, res: Response) {
+
     }
 }
 
--- routes/auth.routes.ts
+++ routes/auth.routes.ts
@@ -7,5 +7,8 @@ const controller = new AuthController();
 
 router.post("/login", controller.login);
 router.post("/register", controller.register);
+router.get("/profile", controller.profile)
+router.post('/email-available/{email}', controller.email_available)
+router.post('/username-available/{email}', controller.username_available)
 
 export const auth_routes = router
