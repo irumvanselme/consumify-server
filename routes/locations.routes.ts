bbe056e629bafe7508bfac4c62fcc824e33c3696
irumvanselme <andesanselme@gmail.com>
2021-06-11 20:46:39 +0200

    Finish view by id and update of a location

--- routes/locations.routes.ts
+++ routes/locations.routes.ts
@@ -5,6 +5,8 @@ const router = Router()
 const controller = new LocationsController()
 
 router.get("/", controller.get_all)
+router.get("/:id", controller.show)
 router.post("/", controller.create)
+router.put("/:id", controller.update)
 
 export const locations_routes = router
