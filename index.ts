270f4ff62c8dbbafcd83e047f432ad769dff79c1
irumvanselme <andesanselme@gmail.com>
2021-06-11 20:52:04 +0200

    Change the running console world

--- index.ts
+++ index.ts
@@ -7,19 +7,15 @@ import routes from "./routes"
 
 const app: express.Application = express()
 
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.get("/", (req: express.Request, res: express.Response)=> {
+app.get("/", (req: express.Request, res: express.Response) => {
     res.sendFile(join(resolve(), "/views/welcome.html"))
 })
 
 app.use("/api", routes)
 
 app.listen(process.env.PORT, () =>
-    console.log(
-        `[${new Date().toUTCString()}] Development Server 1  (http://localhost:${
-            process.env.PORT
-        }) started`
-    )
+    console.log(`[${new Date().toUTCString()}] Development Server running on  http://localhost:${process.env.PORT} started`)
 );
