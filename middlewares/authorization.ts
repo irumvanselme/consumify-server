a3db67a2bcaf9bfa6a1f409cd1f5501faec4705e
irumvanselme <andesanselme@gmail.com>
2021-06-11 15:27:52 +0200

    Make the Authorization Method

--- middlewares/authorization.ts
+++ middlewares/authorization.ts
@@ -1,12 +1,14 @@
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Response } from "express";
 import jwt from "jsonwebtoken"
+import { RequestWithUser } from "../interfaces/RequestWithUser";
+import { User } from "../interfaces/User";
 
-export function authorizationMiddleware(req: Request, res: Response, next: NextFunction){
+export function authorizationMiddleware(req: RequestWithUser, res: Response, next: NextFunction){
     const token = req.header('x-auth-token')
     if(!token) return res.send('token missing..').status(401)
 
     try {
-        req.user = jwt.verify(token, process.env.JWT_KEY as string)
+        req.user = jwt.verify(token, process.env.JWT_KEY as string) as User
         next()
     } catch (err) {
         return  res.send('invalid token').status(403)
